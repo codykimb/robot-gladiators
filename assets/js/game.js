d21d0d483a652c1e8548c9d5f7dd54144c929678
Cody Kimb <codykimbell@gmail.com>
2020-12-16 15:32:08 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -34,14 +34,14 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
-
+        playerMoney = Math.max(0, playerMoney - 10);
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack -3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -60,7 +60,8 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -93,7 +94,7 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
@@ -115,7 +116,7 @@ var startGame = function() {
   // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
   endGame();
 };
-// function to end the entire game
+
 var endGame = function() {
   if (playerHealth > 0) {
     window.alert("Great job, you survived the game! You now have a score of " + playerMoney + ".");
@@ -170,6 +171,11 @@ var shop = function() {
       shop();
       break;
   }
+};
+
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min +1) + min);
+  return value;
 };
 
 // start the game when the page loads
